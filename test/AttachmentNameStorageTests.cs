9bfd1da8bed3be19225c8dbc364bcf219f2fd106
Takács Leontin <71851708+Tetrodotoxin13@users.noreply.github.com>
2021-06-02 12:19:27 +0200

    Add files via upload

---
+++ test/AttachmentNameStorageTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeaponManager.validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponManager.validation.Tests
+{
+    [TestClass()]
+    public class AttachmentNameStorageTests
+    {
+        #region Good test
+        /// <summary>
+        /// Test if the storage is not empty
+        /// </summary>
+        [TestMethod()]
+        public void AttachmentNameStorageTestAttachmentNameisCorrect()
+        {
+            try
+            {
+                AttachmentNameStorage ats = new AttachmentNameStorage("Stippled Griptape");
+                if (ats.isAttachemntnameValid())
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The attachment name is valid but unfortunatelly something wrong");
+            }
+        }
+
+        /// <summary>
+        /// Test if the storage is not empty
+        /// </summary>
+        [TestMethod()]
+        public void AttachmentNameStorageTestAttachmentNameisCorrect2()
+        {
+            try
+            {
+                AttachmentNameStorage ats = new AttachmentNameStorage("CombatGriptape");
+                if (ats.isAttachemntnameValid())
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The attachment name is valid but unfortunatelly something wrong");
+            }
+        }
+
+        /// <summary>
+        /// Test if the storage is not empty
+        /// </summary>
+        [TestMethod()]
+        public void AttachmentNameStorageTestAttachmentNameisCorrect3()
+        {
+            try
+            {
+                AttachmentNameStorage ats = new AttachmentNameStorage("combatgriptape");
+                if (ats.isAttachemntnameValid())
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The attachment name is valid but unfortunatelly something wrong");
+            }
+        }
+
+        /// <summary>
+        /// Test if the storage is not empty
+        /// </summary>
+        [TestMethod()]
+        public void AttachmentNameStorageTestAttachmentNameisCorrect4()
+        {
+            try
+            {
+                AttachmentNameStorage ats = new AttachmentNameStorage("combat griptape");
+                if (ats.isAttachemntnameValid())
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The attachment name is valid but unfortunatelly something wrong");
+            }
+        }
+        #endregion
+
+        #region Bad test
+        /// <summary>
+        /// test if the storage is empty
+        /// </summary>
+        [TestMethod()]
+        public void AttachmentNameStorageTestAttachmentNameisEmpty()
+        {
+            try
+            {
+                AttachmentNameStorage ats = new AttachmentNameStorage("");
+                if (ats.isAttachemntnameValid())
+                {
+                    Assert.Fail("Accept the empty attachment name");
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ test/WeaponNameStorageTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeaponManager.validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponManager.validation.Tests
+{
+    [TestClass()]
+    public class WeaponNameStorageTests
+    {
+        #region Good test 
+        /// <summary>
+        /// test if the weapon name storage is good and not empty
+        /// </summary>
+        [TestMethod()]
+        public void isWeaponnameValidTestWeaponNameCorrect()
+        {
+            try
+            {
+                WeaponNameStorage wns = new WeaponNameStorage("AK-47");
+                if (wns.isWeaponnameValid()) 
+                {
+                    return;
+                }
+            }
+            catch (Exception) 
+            {
+                Assert.Fail("The weapon name is valid but unfortunatelly something wrong");
+            }
+        }
+
+        /// <summary>
+        /// test if the weapon name storage is good and not empty
+        /// </summary>
+        [TestMethod()]
+        public void isWeaponnameValidTestWeaponNameCorrect2()
+        {
+            try
+            {
+                WeaponNameStorage wns = new WeaponNameStorage("Grau");
+                if (wns.isWeaponnameValid())
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The weapon name is valid but unfortunatelly something wrong");
+            }
+        }
+
+
+        /// <summary>
+        /// test if the weapon name storage is good and not empty
+        /// </summary>
+        [TestMethod()]
+        public void isWeaponnameValidTestWeaponNameCorrect3()
+        {
+            try
+            {
+                WeaponNameStorage wns = new WeaponNameStorage("toz-8");
+                if (wns.isWeaponnameValid())
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The weapon name is valid but unfortunatelly something wrong");
+            }
+        }
+        #endregion
+
+        #region Bad test
+        /// <summary>
+        /// test if the weapon name storage is empty
+        /// </summary>
+        [TestMethod()]
+        public void isWeaponnameValidTestWeaponNameIsEmpty() 
+        {
+            try
+            {
+                WeaponNameStorage wns = new WeaponNameStorage("");
+                if (wns.isWeaponnameValid())
+                {
+                    Assert.Fail("Accept the empty Weapon name");
+                }
+            }
+            catch (WeaponManager.WeaponNamseisEmptyException) 
+            {
+                return;
+            }
+        }
+        #endregion
+
+    }
+}
